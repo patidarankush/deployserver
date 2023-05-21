f1925ca159580213fa08ed2cc2e193b22de056e5
patidarankush <patidarankush59@gmail.com>
2023-05-21 18:32:25 +0530

    server changes

--- server.js
+++ server.js
@@ -1,51 +1,24 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3030;
 
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
+// your code
 
-const server = express();
-
-server.use(express.json());
-
-// Serve static files
-server.use(express.static('public'));
-
-
-server.post('/', (req, res) => {
-  const sensorData = req.body;
-  // Process the received sensorData here
-  console.log(sensorData); // Example: Log the received data
-
-  // Send a response back to the ESP32
-  res.send('Data received successfully');
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
 
-server.get('/', (req, res) => {
-  const responseData = {
-    message: 'Data sent successfully from laptop',
-  };
+app.use(express.json()); // Parse JSON request bodies
 
-  res.json(responseData);
+app.post('/data', (req, res) => {
+  const sensorData = req.body; // Access the data sent by ESP32
+  // Process the data and perform any necessary actions
+  console.log('Received data:', sensorData);
+  res.sendStatus(200); // Send a response back to ESP32
 });
 
-// code changes to get data from ESP32
-let sensorData = {}; // Variable to store the sensor data
-server.post('/data', (req, res) => {
-  sensorData = req.body; // Store the received sensor data
-  console.log(sensorData); // Log the received data
-
-  res.send('Data received successfully');
-});
-
-server.get('/data', (req, res) => {
-  res.json(sensorData); // Serve the stored sensor data to the client
-}); 
-
-
-https.createServer(options, server).listen(3000, () => {
-  console.log('Server is running on port', 3000, '(HTTPS)');
+// Start the server
+// const port = 3000; // Use any available port
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
